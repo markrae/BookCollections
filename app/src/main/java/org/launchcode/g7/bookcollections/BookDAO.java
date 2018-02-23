7d7c8cc63e6a2f52760531eb69dea7091b4eb0a6
Mark Rae <rae.mark@gmail.com>
2018-02-22 19:02:40 -0800

    Added Room Fiddled with fragments

---
+++ app/src/main/java/org/launchcode/g7/bookcollections/BookDAO.java
@@ -0,0 +1,32 @@
+package org.launchcode.g7.bookcollections;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+/**
+ * Created by Mark Rae on 2/22/2018.
+ */
+
+@Dao
+public interface BookDAO
+{
+    @Query("SELECT * FROM Book")
+    List<Book> getAll();
+
+    @Query("SELECT * FROM Book WHERE uid IN (:userIds)")
+    List<Book> loadAllByIds(int[] userIds);
+
+    @Query("SELECT * FROM Book WHERE first_name LIKE :first AND "
+            + "last_name LIKE :last LIMIT 1")
+    Book findByName(String first, String last);
+
+    @Insert
+    void insertAll(Book... books);
+
+    @Delete
+    void delete(Book book);
+}
--- app/src/main/java/org/launchcode/g7/bookcollections/MainActivity.java
+++ app/src/main/java/org/launchcode/g7/bookcollections/MainActivity.java
@@ -55,5 +55,8 @@ public class MainActivity extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
+
     }
+
+
 }
