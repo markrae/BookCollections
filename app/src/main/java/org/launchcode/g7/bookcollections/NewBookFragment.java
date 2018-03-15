2e12431b5c5b082917925d1ea4301b4423c5a547
Adam Duhamel <Adam.Duhamel1996@gmail.com>
2018-03-15 00:44:24 -0700

    added an on click listener to the fragments that grabs the data from the edit text and converts it to string, added TODO that say to do things with said data. then it removes the fragment

--- app/src/main/java/org/launchcode/g7/bookcollections/NewBookFragment.java
+++ app/src/main/java/org/launchcode/g7/bookcollections/NewBookFragment.java
@@ -6,12 +6,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewBookFragment extends Fragment{
+    private Button addBook;
+    private EditText editText;
+    private String ISBN;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new_book, container, false);
+
+        editText = view.findViewById(R.id.bookISBN);
+        addBook = view.findViewById(R.id.addBook);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ISBN = editText.getText().toString();
+                //TODO do something with ISBN before removing fragment
+                removeSelf();
+            }
+        };
+        addBook.setOnClickListener(listener);
+
         return view;
+    }
+
+    private void removeSelf() {
+        getActivity().getSupportFragmentManager().beginTransaction().remove(this).commit();
     }
 }
--- app/src/main/java/org/launchcode/g7/bookcollections/NewShelfFragment.java
+++ app/src/main/java/org/launchcode/g7/bookcollections/NewShelfFragment.java
@@ -6,13 +6,42 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.launchcode.g7.bookcollections.models.Shelf;
 
 public class NewShelfFragment extends Fragment {
+    private Button buildShelf;
+    private EditText shelfEdit;
+    private String shelfName;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new_shelf, container, false);
+
+        shelfEdit = view.findViewById(R.id.shelfName);
+        buildShelf = view.findViewById(R.id.buildShelf);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shelfName = shelfEdit.getText().toString();
+                createNewShelf();
+                removeSelf();
+            }
+        };
+        buildShelf.setOnClickListener(listener);
+
         return view;
+    }
+
+    private void createNewShelf() {
+        Shelf shelf = new Shelf(shelfName);
+        //TODO add this shelf to the list of shelves and save it to file
+    }
+
+    private void removeSelf() {
+        getActivity().getSupportFragmentManager().beginTransaction().remove(this).commit();
     }
 }
